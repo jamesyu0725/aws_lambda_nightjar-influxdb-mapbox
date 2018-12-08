859de14fcc9eae83930f312bd6abefec1a302bbe
James Yu <jamesyu0725@gmail.com>
2018-12-08 15:47:22 -0600

    update Test script

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send({
-    "geometry": 
-      {"type": "Point", "coordinates": [127.19055820174076, -21.89972486964625]},
-     "type": "Feature", 
-     "properties": {}
+  res.send(
+    {
+    "geometry": {
+        "type": "Point", 
+        "coordinates": [127.19055820174076, -21.89972486964625]},
+        "type": "Feature", 
+        "properties": {}
   });
 });
 
