4397fa9b2168f14041094a512bf977d769956b5d
James Yu <jamesyu0725@gmail.com>
2018-12-08 20:32:30 -0600

    remove test

--- tests/test.js
+++ tests/test.js
@@ -9,16 +9,15 @@ const request = supertest(app);
 describe('Tests app', function() {
   it('verifies get', function(done) {
     request.get('/').expect(200).end(function(err, result) {
-        //test.string(resu
-        lt.body.type).contains('Point');
-        test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
+        //test.string(result.body.type).contains('Point');
+        //test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
         done(err);
     });
   });
   it('verifies post', function(done) {
     request.post('/').expect(200).end(function(err, result) {
         //test.string(result.body.Output).contains('Hello');
-        test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
+        //test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
         done(err);
     });
   });
