f7c391c0dc4b49e9c392b8e9357558a2b6a04443
James Yu <jamesyu0725@gmail.com>
2018-12-08 20:02:12 -0600

    update

--- tests/test.js
+++ tests/test.js
@@ -9,7 +9,7 @@ const request = supertest(app);
 describe('Tests app', function() {
   it('verifies get', function(done) {
     request.get('/').expect(200).end(function(err, result) {
-        test.string(result.body.Output).contains('coordinates');
+        test.string(result.body.type).contains('Point');
         test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
         done(err);
     });
