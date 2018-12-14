5e32ae60848cc4d1964571c442cafda29c2cb15c
James Yu <jyu@centrexsolutions.com>
2018-12-13 22:03:37 -0600

    update

--- app.js
+++ app.js
@@ -58,15 +58,18 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function(req, res) {
-  var latitude = "";
-  var longitude = "";
-  influx.query(`
+  var latitude = "0";
+  var longitude = "0";
+  var test = "0";
+  var result = influx.query(`
     SELECT last(\"latitude1\") AS \"latitude\", last(\"longitude1\") AS \"longitude\", time 
     FROM \"DHS\".\"autogen\".\"gas-field_stm-001\" where time > now() - 240h and \"latitude1\" <> 0
-  `).then(data => {
-    latitude = data.results[0].series[0].values[0][0];
-    longitude = data.results[0].series[0].values[0][1];
-  });  
+  `)
+  
+  latitude = result.results[0].series[0].values[0][0];
+  longitude = result.results[0].series[0].values[0][1];
+  test = JSON.parse(JSON.stringify(result));  
+
   res.send(
     {
     "geometry": {
@@ -74,8 +77,8 @@ app.get('/', function(req, res) {
         "coordinates": [latitude, longitude]},
         "type": "Feature", 
         "properties": {
-            "node":"test"
-        }
+            "node": test
+          }
   });
 });
 
